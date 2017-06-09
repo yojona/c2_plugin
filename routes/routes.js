8ffc7027cf7271297704cade1ef3662b2ce5fa43
gaby cruz olivera <gaby cruz olivera>
2017-06-08 23:30:59 -0700

    rutas agregadas

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var voucher_codes = require('voucher-code-generator');
+var shortid = require('shortid');
+const bcrypt = require('bcrypt');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +14,7 @@ module.exports = router;
 router.post('/:appId/level', function(req, res, next){
   try{
     var reqObj = req.body;
+    var shortCode = shortid.generate();
     console.log(req.get('appId'));
 
     req.getConnection(function(err, conn){
@@ -25,10 +28,7 @@ router.post('/:appId/level', function(req, res, next){
         var insertValues = {
           "app_id": req.param('appId'),
           "user_id": reqObj.userId,
-          "code": voucher_codes.generate({
-              length: 9,
-              count: 1
-          }),
+          "code": shortCode,
           "title": reqObj.title,
           "body": reqObj.body
         };
@@ -39,8 +39,8 @@ router.post('/:appId/level', function(req, res, next){
             return next(err);
           }
           console.log(result);
-          var levelId = result.levelId;
-          res.json({'Level_id:': levelId});
+
+          res.json({'code: ': shortCode});
         });
       }
     });
@@ -77,6 +77,112 @@ router.get('/:appid/level/:code', function(req, res, next){
           }
           res.json(resLev);
         });
+      }
+    });
+  }catch(ex){
+      console.error('Internal error:'+ ex);
+      return next(ex);
+  }
+});
+
+//User routes
+router.post('/users', function(req, res, next){
+  try{
+    var reqObj = req.body;
+    var password;
+    password = bcrypt.hashSync(reqObj.password, 10);
+
+    req.getConnection(function(err, conn){
+      if(err){
+        console.error('SQl connection error: ', err);
+        return next(err);
+      }
+      else{
+
+        var insertSql = "INSERT INTO user SET ?";
+        var insertValues = {
+          "firstname": reqObj.firstname,
+          "lastname": reqObj.lastname,
+          "username": reqObj.username,
+          "email": reqObj.email,
+          "password": password,
+          "country": reqObj.country,
+          "developer": reqObj.developer,
+          "status": reqObj.status
+        };
+
+        var query = conn.query(insertSql, insertValues, function(err, result){
+          if(err){
+            console.error('SQL error: ', err);
+            res.json({'success' : false, 'error': err});
+            return next(err);
+          }
+          console.log(result);
+
+          res.status(201).json({'success': true});
+        });
+      }
+    });
+  }catch(ex){
+      console.error("Internal error:"+ex);
+      return next(ex);
+  }
+});
+
+var authApp = function(req, res, next, appname, appkey){
+  try{
+    req.getConnection(function(err, conn){
+      if(err){
+        console.error('SQL connection error: ', err);
+        return next(err);
+      }else{
+
+        conn.query('select app_name, app_key from app where app_key = ? ', [appkey], function(err,rows, fields){
+          if(err){
+            console.error('SQL error: ', err);
+            return next(err);
+          }
+          var res;
+          for( var levIndex in rows){
+            var levObj = rows[levIndex];
+            console.log(levObj);
+            res.push(levObj);
+          }
+          res.json({'resultado': resLev, 'auth': true});
+        });
+      }
+    });
+  }catch(ex){
+    console.error('Internal error:'+ ex);
+    return next(ex);
+  }
+};
+
+router.get('/auth', function(req, res, next){
+  try{
+    var appname = req.param('appname');
+    var appkey = req.param('appkey');
+    authApp(req,res,next,appname, appkey);
+
+    req.getConnection(function(err, conn){
+      if(err){
+        console.error('SQL connection error: ', err);
+        return next(err);
+      }else{
+        /*
+        conn.query('select username, title, body from level inner join user on level.user_id = user.id where code = ? ', [code], function(err,rows, fields){
+          if(err){
+            console.error('SQL error: ', err);
+            return next(err);
+          }
+          var resLev = [];
+          for( var levIndex in rows){
+            var levObj = rows[levIndex];
+            console.log(levObj);
+            resLev.push(levObj);
+          }
+          res.json(resLev);
+        });*/
       }
     });
   }catch(ex){
